4b95e477d9e751c8d33dbd5b3315881c425f6f5a
steven nevins <steven.nevins.js@gmail.com>
2022-09-19 11:15:35 -0400

    initial commit

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,34 +1,29 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import {ERC165Checker} from "openzeppelin-contracts/contracts/utils/introspection/ERC165Checker.sol";
 import {ERC1155, ERC1155TokenReceiver} from "solmate/tokens/ERC1155.sol";
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
-import {IERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
-import {ERC1155B} from "../test/mocks/ERC1155B.sol";
 
 import {ICurve, Pair} from "./interfaces/ICurve.sol";
 import {TransferLib} from "./lib/TransferLib.sol";
 
 /// a minimalistic meta AMM
 contract Vault is ERC1155, ERC1155TokenReceiver {
-    using ERC165Checker for address;
     using TransferLib for address;
     /// metadata stuff for 1155
     string public constant name = "";
     string public constant symbol = "";
-
-    bytes4 public constant IERC20_ID = type(IERC20).interfaceId;
-    bytes4 public constant IERC721_ID = type(IERC721).interfaceId;
-    bytes4 public constant IERC1155_ID = type(IERC1155).interfaceId;
-    bytes4 public constant ERC1155B_ID = type(ERC1155B).interfaceId;
 
     function uri(uint256) public pure override returns (string memory) {
         return "";
     }
 
     mapping(uint256 => Pair) public pairs; /// pairId => Pair
+
+    /// I think the sauce will be allowing people to scope their own ids
+    /// maybe let people deposit to a Reserve where the hash is based on the address
+    /// they're giving controll over their ids ie either Jitty or themselves
+    // mapping(uint256 => Reserve) public internalBalances; ///
+
     mapping(address => mapping(uint256 => uint256)) public enumeratedIds; /// token adddress => index => id held by Vault, balanceOf(address(this)) = current index
     mapping(address => uint128) public currentIndex; /// for ERC1155B: token address => current Index
 
@@ -37,10 +32,9 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         uint128 token0Amount,
         uint128 token1Amount,
         uint128 minK,
-        bytes calldata pairData,
-        bytes calldata transferData
+        bytes calldata pairData
     ) external returns (uint128 k) {
-        k = _addLiquidity(to, token0Amount, token1Amount, pairData, transferData);
+        k = _addLiquidity(to, token0Amount, token1Amount, pairData);
 
         require(k >= minK, "liquidity must be greater than minK liquidity");
     }
@@ -63,10 +57,9 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         address tokenIn,
         uint128 amountIn,
         uint128 minAmountOut,
-        bytes calldata pairData,
-        bytes calldata transferData
+        bytes calldata pairData
     ) external returns (address tokenOut, uint128 amountOut) {
-        (tokenOut, amountOut) = _swap(to, tokenIn, amountIn, pairData, transferData);
+        (tokenOut, amountOut) = _swap(to, tokenIn, amountIn, pairData);
 
         require(amountOut >= minAmountOut, "amountOut must be greater than minAmountOut");
     }
@@ -75,15 +68,14 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         address to,
         uint128 token0Amount,
         uint128 token1Amount,
-        bytes calldata pairData,
-        bytes calldata transferData
+        bytes calldata pairData
     ) internal returns (uint128 k) {
         uint256 pairId = uint256(keccak256(pairData));
         Pair storage pair = pairs[pairId];
 
-        (address token0, , , address token1, , , ICurve invariant) = abi.decode(
+        (address token0, , address token1, , ICurve invariant) = abi.decode(
             pairData,
-            (address, uint256, bytes4, address, uint256, bytes4, ICurve)
+            (address, uint256, address, uint256, ICurve)
         );
         require(token0 < token1, "tokens must be sorted");
 
@@ -92,14 +84,9 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         pair.reserve0 += token0Amount;
         pair.reserve1 += token1Amount;
 
-        (bytes memory token0Data, bytes memory token1Data) = abi.decode(
-            transferData,
-            (bytes, bytes)
-        );
-
         _mint(to, pairId, k, "");
-        _transfer(token0, to, address(this), token0Amount, pairData, token0Data);
-        _transfer(token1, to, address(this), token1Amount, pairData, token1Data);
+        _transfer(token0, to, address(this), token0Amount, pairData);
+        _transfer(token1, to, address(this), token1Amount, pairData);
     }
 
     function _removeLiquidity(
@@ -110,9 +97,9 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         uint256 pairId = uint256(keccak256(pairData));
         Pair storage pair = pairs[pairId];
 
-        (address token0, , , address token1, , , ICurve invariant) = abi.decode(
+        (address token0, , address token1, , ICurve invariant) = abi.decode(
             pairData,
-            (address, uint256, bytes4, address, uint256, bytes4, ICurve)
+            (address, uint256, address, uint256, ICurve)
         );
         require(token0 < token1, "tokens must be sorted");
         require(address(invariant).code.length != 0, "amm must be a contract");
@@ -121,22 +108,21 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         pair.reserve1 -= amount1Out;
 
         _burn(from, pairId, k);
-        _transfer(token0, address(this), from, amount0Out, pairData, "");
-        _transfer(token1, address(this), from, amount1Out, pairData, "");
+        _transfer(token0, address(this), from, amount0Out, pairData);
+        _transfer(token1, address(this), from, amount1Out, pairData);
     }
 
     function _swap(
         address to,
         address tokenIn,
         uint128 amountIn,
-        bytes calldata pairData,
-        bytes calldata transferData
+        bytes calldata pairData
     ) internal returns (address tokenOut, uint128 amountOut) {
         uint256 pairId = uint256(keccak256(pairData));
         Pair storage pair = pairs[pairId];
-        (address token0, , , address token1, , , ICurve invariant) = abi.decode(
+        (address token0, , address token1, , ICurve invariant) = abi.decode(
             pairData,
-            (address, uint256, bytes4, address, uint256, bytes4, ICurve)
+            (address, uint256, address, uint256, ICurve)
         );
 
         require(token0 < token1, "tokens must be sorted");
@@ -149,8 +135,8 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
 
         (token0 == tokenIn) ? pair.reserve0 += amountIn : pair.reserve0 -= amountOut;
 
-        _transfer(tokenIn, to, address(this), amountIn, pairData, transferData);
-        _transfer(tokenOut, address(this), to, amountOut, pairData, "");
+        _transfer(tokenIn, to, address(this), amountIn, pairData);
+        _transfer(tokenOut, address(this), to, amountOut, pairData);
     }
 
     function _transfer(
@@ -158,71 +144,15 @@ contract Vault is ERC1155, ERC1155TokenReceiver {
         address from,
         address to,
         uint128 amount,
-        bytes calldata pairData,
-        bytes memory transferData
+        bytes calldata pairData
     ) internal {
         uint256 tokenId;
-        (
-            address token0,
-            uint256 token0Id,
-            bytes4 token0InterfaceId,
-            ,
-            uint256 token1Id,
-            bytes4 token1InterfaceId,
-
-        ) = abi.decode(pairData, (address, uint256, bytes4, address, uint256, bytes4, ICurve));
-
-        bytes4 interfaceId = (token == token0) ? token0InterfaceId : token1InterfaceId;
-        if (interfaceId == IERC20_ID) {
-            if (from == address(this)) {
-                token._performERC20Transfer(to, amount);
-            } else {
-                token._performERC20TransferFrom(from, to, amount);
-            }
-        } else if (interfaceId == IERC721_ID) {
-            if (from == address(this)) {
-                uint256 upper = IERC721(token).balanceOf(from);
-                uint256 lower = upper - amount;
-                for (; lower < upper; lower++) {
-                    token._performERC721Transfer(from, to, lower);
-                }
-            } else {
-                uint256[] memory ids = abi.decode(transferData, (uint256[]));
-                uint256 length = ids.length;
-                uint256 totalIds = IERC721(token).balanceOf(to);
-                for (uint256 i; i < length; i++) {
-                    tokenId = ids[i];
-                    enumeratedIds[token][totalIds++] = tokenId;
-                    token._performERC721Transfer(from, to, tokenId);
-                }
-            }
-        } else if (interfaceId == IERC1155_ID) {
-            tokenId = (token == token0) ? token0Id : token1Id;
-            token._performERC1155Transfer(from, to, tokenId, amount, transferData);
-        } else if (interfaceId == ERC1155B_ID) {
-            if (from == address(this)) {
-                uint256[] memory ids = new uint256[](uint256(amount));
-                uint256[] memory amounts = new uint256[](uint256(amount));
-                uint256 upper = currentIndex[token];
-                uint256 lower = upper - amount;
-                currentIndex[token] = uint128(lower);
-                for (uint256 i; lower < upper; lower++) {
-                    ids[i] = enumeratedIds[token][lower];
-                    amounts[i++] = 1;
-                }
-                token._performERC1155BatchTransfer(from, to, ids, amounts, "");
-            } else {
-                (uint256[] memory ids, uint256[] memory amounts) = abi.decode(
-                    transferData,
-                    (uint256[], uint256[])
-                );
-                uint256 length = ids.length;
-                uint256 totalIds = currentIndex[token];
-                for (uint256 i; i < length; i++) enumeratedIds[token][totalIds++] = ids[i];
-                currentIndex[token] += uint128(length);
-                token._performERC1155BatchTransfer(from, to, ids, amounts, "");
-            }
-        } else revert("token must support ERC20, ERC721, or ERC1155");
+        (address token0, uint256 token0Id, , uint256 token1Id, ) = abi.decode(
+            pairData,
+            (address, uint256, address, uint256, ICurve)
+        );
+        tokenId = (token == token0) ? token0Id : token1Id;
+        token._performERC1155Transfer(from, to, tokenId, amount, "");
     }
 
     /// @notice Handles the receipt of a single ERC1155 token type
--- test/VaultERC1155.t.sol
+++ test/VaultERC1155.t.sol
@@ -8,112 +8,64 @@ import "../src/Vault.sol";
 import "../src/invariants/XtYK.sol";
 
 contract VaultERC1155Test is Test {
-    bytes4 public constant ERC20_INTERFACE_ID = type(IERC20).interfaceId;
-    bytes4 public constant ERC1155_INTERFACE_ID = type(IERC1155).interfaceId;
     Vault public vault;
     XtYK public xyk;
     MockERC1155 public token0;
-    MockERC20 public token1;
-    uint256 computedPairId;
+    MockERC1155 public token1;
+    uint256 public computedPairId;
 
     function setUp() public {
         vault = new Vault();
         xyk = new XtYK();
         token0 = new MockERC1155();
-        token1 = new MockERC20("", "", 18);
+        token1 = new MockERC1155();
         token0.mint(address(0xBEEF), 1, type(uint128).max);
-        token1.mint(address(0xBEEF), type(uint128).max);
+        token1.mint(address(0xBEEF), 1, type(uint128).max);
         vm.startPrank(address(0xBEEF));
         token0.setApprovalForAll(address(vault), true);
-        token1.approve(address(vault), type(uint256).max);
+        token1.setApprovalForAll(address(vault), true);
         vm.stopPrank();
     }
 
-    function testCreatePairERC1155ERC20() public {
+    function testCreatePairERC1155ERC1155() public {
         assertTrue(address(token0) < address(token1));
 
         computedPairId = uint256(
-            keccak256(
-                abi.encode(
-                    address(token0),
-                    1,
-                    ERC1155_INTERFACE_ID,
-                    address(token1),
-                    0,
-                    ERC20_INTERFACE_ID,
-                    address(xyk)
-                )
-            )
+            keccak256(abi.encode(address(token0), 1, address(token1), 1, address(xyk)))
         );
     }
 
-    function testAddLiquidityERC1155ERC20() public {
-        testCreatePairERC1155ERC20();
+    function testAddLiquidityERC1155ERC1155() public {
+        testCreatePairERC1155ERC1155();
 
-        bytes memory pairData = abi.encode(
-            address(token0),
-            1,
-            ERC1155_INTERFACE_ID,
-            address(token1),
-            0,
-            ERC20_INTERFACE_ID,
-            address(xyk)
-        );
-
-        bytes memory data = abi.encode("", "");
+        bytes memory pairData = abi.encode(address(token0), 1, address(token1), 1, address(xyk));
 
         vm.prank(address(0xBEEF));
-        vault.addLiquidity(address(0xBEEF), 1 gwei, 1 gwei, 1, pairData, data);
+        vault.addLiquidity(address(0xBEEF), 1 gwei, 1 gwei, 1, pairData);
         assertEq(token0.balanceOf(address(vault), 1), 1 gwei);
-        assertEq(token1.balanceOf(address(vault)), 1 gwei);
+        assertEq(token1.balanceOf(address(vault), 1), 1 gwei);
     }
 
     function testSwapERC20toERC1155() public {
-        testAddLiquidityERC1155ERC20();
+        testAddLiquidityERC1155ERC1155();
 
-        bytes memory pairData = abi.encode(
-            address(token0),
-            1,
-            ERC1155_INTERFACE_ID,
-            address(token1),
-            0,
-            ERC20_INTERFACE_ID,
-            address(xyk)
-        );
+        bytes memory pairData = abi.encode(address(token0), 1, address(token1), 1, address(xyk));
 
         vm.prank(address(0xBEEF));
-        vault.swap(address(0xBEEF), address(token1), 0.5 gwei, 1, pairData, "");
+        vault.swap(address(0xBEEF), address(token1), 0.5 gwei, 1, pairData);
     }
 
-    function testSwapERC1155toERC20() public {
-        testAddLiquidityERC1155ERC20();
-        bytes memory pairData = abi.encode(
-            address(token0),
-            1,
-            ERC1155_INTERFACE_ID,
-            address(token1),
-            0,
-            ERC20_INTERFACE_ID,
-            address(xyk)
-        );
-
-        bytes memory data = abi.encode("");
+    function testSwapERC1155toERC1155() public {
+        testAddLiquidityERC1155ERC1155();
+        bytes memory pairData = abi.encode(address(token0), 1, address(token1), 1, address(xyk));
 
         vm.prank(address(0xBEEF));
-        vault.swap(address(0xBEEF), address(token0), 0.5 gwei, 0, pairData, data);
+        vault.swap(address(0xBEEF), address(token0), 0.5 gwei, 0, pairData);
     }
 
     function testRemoveLiquidityERC20() public {
-        testAddLiquidityERC1155ERC20();
-        bytes memory pairData = abi.encode(
-            address(token0),
-            1,
-            ERC1155_INTERFACE_ID,
-            address(token1),
-            0,
-            ERC20_INTERFACE_ID,
-            address(xyk)
-        );
+        testAddLiquidityERC1155ERC1155();
+        bytes memory pairData = abi.encode(address(token0), 1, address(token1), 1, address(xyk));
 
         vm.prank(address(0xBEEF));
         vault.removeLiquidity(address(0xBEEF), 1 gwei * 1 gwei - 1000, 1, 1, pairData);
