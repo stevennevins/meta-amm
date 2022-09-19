5142861e09e64e34ec522c8d678b3a136164c423
steven nevins <steven.nevins.js@gmail.com>
2022-09-19 14:05:26 -0400

    layout structs

--- src/lib/ReserveStructs.sol
+++ src/lib/ReserveStructs.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-struct Reserve {
+struct Token {
     address token;
     uint256 identifier;
 }
 
 struct Pair {
+    Token reserve0;
+    Token reserve1;
+    address invariant;
+}
+
+struct Reserves {
     uint128 reserve0;
     uint128 reserve1;
 }
